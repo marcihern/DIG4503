7863f7ed47bde269b453957a5411599454b1eadf
Marcela Hernandez <mhern890@knights.ucf.edu>
2020-01-22 19:05:11 -0500

    Lab 2

---
+++ lab2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './components/HomePage';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <HomePage firstName="Marcela" />
+    );
+  }
+}
+
+export default App;
